40f6bb09a2e0284df38177dbdfdfcc8f756ca88f
lvis <vitaliix@gmail.com>
2018-03-15 11:51:50 +0200

    Markup fix in WidgetSiteBranding

--- src/WidgetSiteBranding.php
+++ src/WidgetSiteBranding.php
@@ -33,7 +33,7 @@ final class WidgetSiteBranding extends Widget
             $siteName = "";
         }
         $markup = '<a href="%1$s" class="custom-logo-link" rel="home">%2$s
-                   <h1 class="site-title">%3$s</h1><br>
+                   <span class="site-title">%3$s</span><br>
                    <small class="site-description hidden-xs">%4$s</small></a>';
         $imageMarkup = wp_get_attachment_image($logoId, WPImages::FULL, false, [
             'class' => 'custom-logo',
