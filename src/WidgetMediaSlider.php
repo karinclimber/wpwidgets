6d121a9623ed21205fc57060a3c01f1b93456b81
lvis <vitaliix@gmail.com>
2018-04-12 10:55:08 +0300

    Add START_SLIDE_ID field in WidgetMediaSlider

--- src/WidgetMediaSlider.php
+++ src/WidgetMediaSlider.php
@@ -115,6 +115,8 @@ final class WidgetMediaSlider extends Widget
             __("Transition speed"), [], 600));
         $this->addField(new WidgetField(WidgetField::NUMBER, self::IMAGE_SCALE_PADDING,
             __("Image padding"), [], 4));
+        $this->addField(new WidgetField(WidgetField::NUMBER, self::START_SLIDE_ID,
+            __("First slide index"), [], 0));
         $this->addField(new WidgetField(WidgetField::IMAGES_WITH_URL, self::IMAGES, __("Images")));
         $this->addField(new WidgetField(WidgetField::SELECT, self::IMAGE_SCALE, __('Image Scale'), [
             self::IMAGE_SCALE_FIT_IF_SMALLER => __('Fit if Smaller'),
