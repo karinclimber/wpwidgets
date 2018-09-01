b10b899df3086a31decfdd966027f543b0df5f4a
lvis <vitaliix@gmail.com>
2018-09-02 00:14:25 +0300

    Some style improvements for post-layoutgrid card more human friendly message of post publishing in category

--- src/post-layoutgrid.php
+++ src/post-layoutgrid.php
@@ -13,15 +13,14 @@
                     <?= get_the_title(); ?>
                 </a>
             </h5>
-            <h6 class="category">
+            <div class="card-description">
                 <?php
+                echo __('in').' ';
                 foreach ((get_the_category()) as $category): ?>
                     <a href="<?= get_term_link($category->cat_ID); ?>" class="text-info">
                         <?= $category->cat_name; ?>
                     </a>
                 <?php endforeach; ?>
-            </h6>
-            <div class="card-description">
                 <time datetime="<?= get_the_modified_time('c'); ?>">
                     <?= \wp\WPUtils::getPostAuthorAndDate(); ?>
                 </time>
